26359dc05f619ad157b493e5f6fee2ca7e21d9d3
Lukas Barth <lukas.barth@kit.edu>
2019-02-07 17:04:30 +0100

    Capture in DRAUP_FOR_EACH. Also, auto-format.

--- draup.hpp
+++ draup.hpp
@@ -5,43 +5,47 @@
 #ifndef DRAUP_DRAUP_HPP
 #define DRAUP_DRAUP_HPP
 
-#include <type_traits>
 #include <boost/hana.hpp>
-#include <boost/hana/set.hpp>
 #include <boost/hana/assert.hpp>
-
+#include <boost/hana/set.hpp>
+#include <type_traits>
 
 namespace draup {
 
 namespace hana = boost::hana;
 
-template<class>
-struct sfinae_true : std::true_type{};
+template <class>
+struct sfinae_true : std::true_type
+{
+};
 
 /* This template will be specialized by every plugin with its own integer.
  * The operator() of the respective struct should return a hana::set of all the
  * classes registered so far. */
-template<unsigned int>
+template <unsigned int>
 struct registry_hook;
 
 /* Methods to test whether a registry_hook with a specified number is already
  * defined via SFINAE. */
-template<class Dummy, unsigned int testN>
+template <class Dummy, unsigned int testN>
 constexpr auto test_existence(int)
--> sfinae_true<decltype(registry_hook<testN>{})>;
-template<class Dummy, unsigned int testN>
+    -> sfinae_true<decltype(registry_hook<testN>{})>;
+template <class Dummy, unsigned int testN>
 constexpr auto test_existence(long) -> std::false_type;
 
 /* Returns the smallest number N for which registry_hook<N> is not defined yet.
- * A per-plugin unique dummy class is necessary s.t. this struct is re-instantiated for
- * every plugin.
+ * A per-plugin unique dummy class is necessary s.t. this struct is
+ * re-instantiated for every plugin.
  */
-template<class Dummy, unsigned int testN = 0>
-constexpr unsigned int get_free_N() {
+template <class Dummy, unsigned int testN = 0>
+constexpr unsigned int
+get_free_N()
+{
 	/* The 'constexpr' is essential here. Otherwise, the template in the
 	 * else-branch must be instantiated even if the condition evaluates to true,
 	 * leading to infinite recursion. */
-	if constexpr (std::is_same<decltype(test_existence<Dummy, testN>(0)), std::false_type>::value) {
+	if constexpr (std::is_same<decltype(test_existence<Dummy, testN>(0)),
+	                           std::false_type>::value) {
 		return testN;
 	} else {
 		return get_free_N<Dummy, testN + 1>();
@@ -49,28 +53,39 @@ constexpr unsigned int get_free_N() {
 }
 
 /* Helper struct / method to chain together the registered classes */
-template<class ClassToRegister, unsigned int myN>
-struct register_class {
+template <class ClassToRegister, unsigned int myN>
+struct register_class
+{
 	/* General case: myN > 0. I.e., other classes have already been registered.
 	 * We recursively get the set of these classes and append our own. */
-	auto operator()() {
-		return hana::insert(registry_hook<myN - 1>{}(), hana::type_c<ClassToRegister>);
+	auto
+	operator()()
+	{
+		return hana::insert(registry_hook<myN - 1>{}(),
+		                    hana::type_c<ClassToRegister>);
 	}
 };
-template<class ClassToRegister>
-struct register_class<ClassToRegister, 0> {
+template <class ClassToRegister>
+struct register_class<ClassToRegister, 0>
+{
 	/* Special case: myN == 0. No other classes have been registered. Create a new
 	 * set. */
-	auto operator()() {
+	auto
+	operator()()
+	{
 		return hana::make<hana::set_tag>(hana::type_c<ClassToRegister>);
 	}
 };
 
-class GetterDummyClass {};
+class GetterDummyClass {
+};
 
-template<class T>
-struct plugin_getter {
-	auto operator()() {
+template <class T>
+struct plugin_getter
+{
+	auto
+	operator()()
+	{
 		constexpr unsigned int max_N = get_free_N<T>() - 1;
 
 		// get the set of registered classes
@@ -86,20 +101,27 @@ struct plugin_getter {
  * Macro to do the necessary incantation to register a new plugin
  */
 
-#define DRAUP_REGISTER(classname)  namespace draup { \
-template<> \
-struct registry_hook<draup::get_free_N<classname>()> { \
-    constexpr static unsigned int my_N = draup::get_free_N<classname>(); \
-    auto operator()() { \
-        return draup::register_class<classname, my_N>{}(); \
-    } \
-};}
+#define DRAUP_REGISTER(classname)                                              \
+	namespace draup {                                                            \
+	template <>                                                                  \
+	struct registry_hook<draup::get_free_N<classname>()>                         \
+	{                                                                            \
+		constexpr static unsigned int my_N = draup::get_free_N<classname>();       \
+		auto                                                                       \
+		operator()()                                                               \
+		{                                                                          \
+			return draup::register_class<classname, my_N>{}();                       \
+		}                                                                          \
+	};                                                                           \
+	}
 
-#define DRAUP_GET_REGISTERED() ::draup::plugin_getter<::draup::GetterDummyClass>{}()
+#define DRAUP_GET_REGISTERED()                                                 \
+	::draup::plugin_getter<::draup::GetterDummyClass>{}()
 
-#define DRAUP_FOR_EACH(plugins, body) boost::hana::for_each(plugins, [](auto plugin_cls) {\
-using plugin = typename decltype(plugin_cls)::type; \
-body; \
-});
+#define DRAUP_FOR_EACH(plugins, body)                                          \
+	boost::hana::for_each(plugins, [&](auto plugin_cls) {                        \
+		using plugin = typename decltype(plugin_cls)::type;                        \
+		body;                                                                      \
+	});
 
-#endif //DRAUP_DRAUP_HPP
+#endif // DRAUP_DRAUP_HPP
